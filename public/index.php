66bae498620e4fd8aa9fdc597bdff6c25d6b4c0f
Ashour Abdal <ashurabdal89@gmail.com>
2024-03-21 20:49:43 +1100

    Connect database

--- public/index.php
+++ public/index.php
@@ -1,10 +1,16 @@
 <?php
 require '../helpers.php';
-
+require basePath('Database.php');
 require basePath('Router.php');
+
+// Instantiating the router
 $router = new Router();
+
+// Get routes
 $routes = require basePath('routes.php');
 
+// Get current URI and HTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
+
 $router->route($uri, $method);
